38e910e4f7dd6bdf5189795c2a71fdab22a769c8
Cam <cambo9p.11@gmail.comgit config  user.email cambo9p.11@gmail.com>
2022-09-07 11:52:07 +1200

    updated function declarations

--- tictactoe.h
+++ tictactoe.h
@@ -6,7 +6,15 @@ typedef struct {
     char player;
 } Move_t;
 
+/* struct to determin the state of the game 
+ */
+typedef struct {
+    char a;
+    char b;
+    char c
+
 void printBoard(Move_t move);
 void checkArgs(int argc, char *argv[]); 
+int checkWin();
 
 #endif // TIKTAKTOE.h
