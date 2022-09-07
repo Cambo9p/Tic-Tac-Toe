0b518d4c4b3e6017ee1c3e1efb344f8d055d9894
Cam <cambo9p.11@gmail.comgit config  user.email cambo9p.11@gmail.com>
2022-09-07 12:26:09 +1200

    player can now make a move and have it show up on board

--- tictactoe.c
+++ tictactoe.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
-#include "tiktaktoe.h"
+#include "tictactoe.h"
 
 char board[10] = {'0','1','2','3','4','5','6','7','8','9'};
+char playerChar;
 
 /* A simple tic tac toe game in c 
  */
 int main(int argc, char *argv[]) {
 
     // check the commandline arguments 
-    checkArgs(argc, &argv);
+    checkArgs(argc, argv);
 
     playerChar = (argv[1] == 0) ? 'X' : 'O';
+    printBoard();
+    playerMove();
+    printBoard();
 
+    return 0;
 }
 
 
@@ -25,32 +31,61 @@ void checkArgs(int argc, char *argv[])
         printf("Incorrect number of arguments");
         exit(0);
     }
-    if (argv[1] > ! || argv[1] < 0) {
+    if ((*argv[1] > 1) & (*argv[1] < 0)) {
         printf("Incorrect argument");
-        exit(0)
+        exit(0);
     }
 }
 
 
+void enterMove(Move_t *move)
+{
+    board[move -> slotNumber] = move -> player;
+}
+
+
+
+/* Function to allow a player to choose a move and add move to board
+ */
+void playerMove()
+{
+    Move_t *move = malloc(1*sizeof(Move_t));
+    printf("Enter a slot to move to\n");
+    scanf("%d", &move->slotNumber); 
+    move-> player = playerChar; 
+    enterMove(move);
+    free(move);
+
+}
+
+/*
+int enemyMove()
+{
+}
+*/
+
+
 /* Prints the Game board once the player has moved 
  */
-void printBoard(Move_t move)
+void printBoard()
 {
     printf("    |    |    \n");
-    printf(" %c | %c | %c \n", board[], board[1], board[2]);
+    printf(" %c  | %c  | %c \n", board[0], board[1], board[2]);
     printf("    |    |    \n");
     printf("----|----|----\n");
     printf("    |    |    \n");
-    printf(" %c | %c | %c \n", board[3], board[4], board[5]);
+    printf(" %c  | %c  | %c \n", board[3], board[4], board[5]);
     printf("    |    |    \n");
     printf("----|----|----\n");
     printf("    |    |    \n");
-    printf(" %c | %c | %c \n", board[6], board[7], board[8]);
+    printf(" %c  | %c  | %c \n", board[6], board[7], board[8]);
     printf("    |    |    \n");
 }
 
+/*
 int checkWin()
 {
 }
+*/
     
 
